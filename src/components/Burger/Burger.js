37bfa0313183ee6b880b3e0d43f36b231d11146b
StefanSimonovic <stefan.simonovic@eng.it>
2019-03-26 14:04:20 +0100

    Burger component, made of dinamicaly added BurgerIngredients conponent is added to BurgerBuilder

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import classes from "./Burger.module.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+
+const burger = props => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(ingrKey => {
+      return [...Array(props.ingredients[ingrKey])].map((_, index) => {
+        return <BurgerIngredient key={ingrKey + index} type={ingrKey} />;
+      });
+    })
+    .reduce((prevArr, curEl) => {
+      return prevArr.concat(curEl);
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please add ingredients!</p>;
+  }
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 import Aux from "../../hoc/Auxiliary/Auxiliary";
+import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      tomato: 0,
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    }
+  };
+
   render() {
     return (
       <Aux>
-        <div>Burger</div>
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Aux>
     );
